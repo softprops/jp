2062f100c8b187dd37b30468e244f1a31a015244
softprops <d.tangren@gmail.com>
2012-08-10 14:06:38 -0400

    a little more documentation

--- src/main/scala/parse.scala
+++ src/main/scala/parse.scala
@@ -14,10 +14,12 @@ object Parse extends JavaTokenParsers {
 
   private val JsonIdentifier = "[$a-zA-Z_][0-9a-zA-Z_$]*".r
 
+  /** base parser for Paths */
   def path: Parser[List[PathElement]] =
     root ~ rep(segment) ^^ 
       { case root ~ segment => root :: segment }
     
+  /** elements of a path query */
   def segment: Parser[PathElement] = (
     subArrayAccess
     | dotProperty
@@ -26,56 +28,72 @@ object Parse extends JavaTokenParsers {
     | descendantAny
     | subscriptProperty)
 
+  /** wild card field */
   def anyField: Parser[Property] = Wildcard ^^ (_ => AnyProperty())
 
+  /** root of path */
   def root: Parser[PathElement] =  complexRoot | bareRoot
 
+  /** The similest root that can be defined */
   def bareRoot: Parser[Property] = Dollar ^^ (_ => Root)
 
+  /** A root and access to its members */
   def complexRoot: Parser[ArrayAccess] = 
     Dollar ~> arraySlice ^^ ((slice) => ArrayAccess(Root, slice))
 
+  /** a named json propery */
   def property: Parser[NamedProperty] =
     jsonident ^^ (NamedProperty(false, _))
 
-  // jtp ident's don't permit digits
+  /** legal json identifier. jtp ident's don't permit digits */
   def jsonident: Parser[String] =
      JsonIdentifier
 
+  /** a property accessed between square brackets */
   def subscriptProperty: Parser[Property] = 
     "['" ~> property <~ "']"
 
+  /** a property accessed by dot reference */
   def dotProperty: Parser[Property] =
     Access ~> property
 
+  /** a named "descendant" propery */
   def descendantProperty: Parser[Property] =
     Descendant ~> property ^^ (p => NamedProperty(true, p.name))
 
+  /** dot reference to any property */
   def dotAny: Parser[AnyProperty] =
     Access + Wildcard ^^ (_ => AnyProperty(false))
 
+  /** descendant referance to any property */
   def descendantAny: Parser[AnyProperty] =
     Descendant + Wildcard ^^ (_ => AnyProperty(true))
 
+  /** any slice of a json array */
   def arraySlice: Parser[ArraySlice] =
-    arraySlice1 | globArraySlice
+    arrayIndex | fullArray
 
-  def arraySlice1: Parser[Segment] =
+  /** reference to an index an array */
+  def arrayIndex: Parser[Segment] =
     "[" ~> wholeNumber <~ "]" ^^ (
       start => Segment(start.toInt, start.toInt + 1, 1)
      )
-
-  def globArraySlice: Parser[ArraySlice] =
+  
+  /** reference to an entire array */
+  def fullArray: Parser[ArraySlice] =
     "[" ~> Wildcard <~ "]" ^^ (_ => All)
 
-  def arrayFieldAccess: Parser[ArrayAccess] =
+  /** access to a slice of an array */
+  def arrayElementAccess: Parser[ArrayAccess] =
     property ~ arraySlice ^^ {
       case property ~ slice => ArrayAccess(property, slice)
     }
 
-  def deepArrayFieldAccess: Parser[ArrayAccess] =
+  /** descendant access to a slice of an array */
+  def descendantArrayElementAccess: Parser[ArrayAccess] =
     Descendant ~> (property ~ arraySlice) ^^ {
-      case NamedProperty(_, name) ~ slice => ArrayAccess(NamedProperty(true, name), slice)
+      case NamedProperty(_, name) ~ slice =>
+        ArrayAccess(NamedProperty(true, name), slice)
     }
 
   def arraySubscriptAccess: Parser[ArrayAccess] =
@@ -84,13 +102,12 @@ object Parse extends JavaTokenParsers {
     }
 
   def arrayAccess: Parser[ArrayAccess] = 
-    arrayFieldAccess | arraySubscriptAccess
+    arrayElementAccess | arraySubscriptAccess
 
-  // .name[index] or ['name'][index] for subacces, for example
-  // $.name[1] or $['name'][0]
   def subArrayAccess: Parser[ArrayAccess] = 
-    (Access ~> arrayFieldAccess) | arraySubscriptAccess | deepArrayFieldAccess
+    (Access ~> arrayElementAccess) | arraySubscriptAccess | descendantArrayElementAccess
 
+  /** given a path specficification, parse out a list of path elements */
   def apply(spec: String): ParseResult[List[PathElement]] =
     parse(path, spec)
 }
